c98023040abefa30c3e85d6a1468ff066967c245
IgorKhromov <chellixc@gmail.com>
2017-07-06 21:58:30 +0300

    fix clear relation

--- fields/types/relationship/RelationshipField.js
+++ fields/types/relationship/RelationshipField.js
@@ -154,7 +154,7 @@ module.exports = Field.create({
 	valueChanged (value) {
 		this.props.onChange({
 			path: this.props.path,
-			value: value,
+			value: value === null ? ' ' : value,
 		});
 	},
 
